46a5f381146ab393cbbd2c110d6596871669b24f
May Thazin Phyo <139356099+MayThazinPhyo24@users.noreply.github.com>
2023-07-12 20:01:42 +0630

    Add files via upload

---
+++ exercise1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int gal,feet;
+    cout<<"Enter a number of gallons";
+    cin>>gal;
+    feet=gal/7.481;
+    cout<<"The equivalent in cubic feet= "<<feet;
+    return 0;
+}
