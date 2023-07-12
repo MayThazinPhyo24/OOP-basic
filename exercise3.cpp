ac14425c2bba447c9f5f7b3322bcf9dc46481e5d
May Thazin Phyo <139356099+MayThazinPhyo24@users.noreply.github.com>
2023-07-12 20:27:04 +0630

    Add files via upload

---
+++ exercise3.cpp
@@ -0,0 +1,17 @@
+/*Write a program that generates the following output
+    10
+    20
+    19 */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int count=10;
+    int a,b;
+    cout<<count<<endl;
+    a=count+10;
+    cout<<a<<endl;
+    b=--a;
+    cout<<b<<endl;
+    return 0;
+}
